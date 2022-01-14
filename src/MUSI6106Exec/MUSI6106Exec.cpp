a9f74c9566e33242771bf8440079c43488b39168
Ian Clester <ijc@ijc8.me>
2022-01-14 18:23:41 -0500

    Check in solution for exercise 1.

--- src/MUSI6106Exec/MUSI6106Exec.cpp
+++ src/MUSI6106Exec/MUSI6106Exec.cpp
@@ -33,21 +33,64 @@ int main(int argc, char* argv[])
 
     //////////////////////////////////////////////////////////////////////////////
     // parse command line arguments
+    if (argc < 3) {
+        std::cerr << "Usage: ./MUSI6106Exec <input wave filename> <output text filename>" << std::endl;
+        return 1;
+    }
+    sInputFilePath = argv[1];
+    sOutputFilePath = argv[2];
  
     //////////////////////////////////////////////////////////////////////////////
     // open the input wave file
- 
+    CAudioFileIf::create(phAudioFile);
+    phAudioFile->openFile(sInputFilePath, CAudioFileIf::FileIoType_t::kFileRead);
+    // and determine number of channels
+    CAudioFileIf::FileSpec_t fileSpec;
+    phAudioFile->getFileSpec(fileSpec);
+    const int numChannels = fileSpec.iNumChannels;
+
     //////////////////////////////////////////////////////////////////////////////
     // open the output text file
+    hOutputFile.open(sOutputFilePath, std::fstream::out);
  
     //////////////////////////////////////////////////////////////////////////////
     // allocate memory
+    ppfAudioData = new float *[numChannels];
+    for (int ch = 0; ch < numChannels; ch++) {
+        ppfAudioData[ch] = new float[kBlockSize];
+    }
  
     //////////////////////////////////////////////////////////////////////////////
     // get audio data and write it to the output text file (one column per channel)
+    bool moreData = true;
+    while (moreData) {
+        long long numFrames = kBlockSize;
+        phAudioFile->readData(ppfAudioData, numFrames);
+        if (numFrames < kBlockSize) {
+            moreData = false;
+        }
+        // Write one line per frame.
+        for (int frame = 0; frame < numFrames; frame++) {
+            // Write one column per channel.
+            for (int ch = 0; ch < numChannels; ch++) {
+                hOutputFile << ppfAudioData[ch][frame];
+                if (ch < numChannels - 1) {
+                    hOutputFile << " ";
+                }
+            }
+            hOutputFile << std::endl;
+        }
+    }
 
     //////////////////////////////////////////////////////////////////////////////
     // clean-up (close files and free memory)
+    hOutputFile.close();
+    for (int ch = 0; ch < numChannels; ch++) {
+        delete ppfAudioData[ch];
+    }
+    delete ppfAudioData;
+    phAudioFile->closeFile();
+    CAudioFileIf::destroy(phAudioFile);
 
     // all done
     return 0;
@@ -63,4 +106,3 @@ void     showClInfo()
 
     return;
 }
-
