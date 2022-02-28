b7a0fd3f16f8836a009dd0c31111a37ac9777831
johnnymac647 <jmcnamara7@gatech.edu>
2022-02-28 12:28:28 -0500

    began setting up Test_Vibrato.cpp file

--- src/Tests/Tests/Test_Vibrato.cpp
+++ src/Tests/Tests/Test_Vibrato.cpp
@@ -3,6 +3,7 @@
 #ifdef WITH_TESTS
 #include "Vector.h"
 #include "Vibrato.h"
+#include "RingBuffer.h"
 
 #include "gtest/gtest.h"
 
@@ -15,6 +16,41 @@ namespace vibrato_test {
             EXPECT_NEAR(buffer1[i], buffer2[i], fTolerance);
         }
     }
+
+//    void CHECK_ZERO_MODULATION(float* buffer1, float* buffer2, int iLength, int iDelayInSamples, float fTolerance)
+//    {
+//        ASSERT_GT(iLength - iDelayInSamples, 0);
+//        for (int i = 0; i < (iLength - iDelayInSamples); i++){
+//            EXPECT_NEAR(buffer1[i], buffer2[i + iDelayInSamples], fTolerance);
+//        }
+//    }
+//
+//    void CHECK_DC_INPUT(){}
+
+    class Vibrato : public  testing::Test{
+    protected:
+        void SetUp();
+        void TearDown();
+        
+    };
+
+    TEST_F(Vibrato, RingBufferTests){
+        //Test 0: Int and float mod functions work as expected
+        //Test 1: Linear Interpolation works as expected
+            //Test 1a: What happens when you try to wrap back around?
+        //Test 2: Read/Write negative index values or values greater than the buffer length
+        //Test 3: Writing a fractional value
+        //Test 4a: Writing when the buffer is full
+        //Test 4b: Reading when the buffer is empty
+        
+        
+    }
+    TEST_F(Vibrato, CheckZeroModulation){}
+    TEST_F(Vibrato, CheckDCInput){}
+    TEST_F(Vibrato, VaryingBlockSize){}
+    TEST_F(Vibrato, ZeroInput){}
+    TEST_F(Vibrato, CustomTest){}
+
 
 }
 
